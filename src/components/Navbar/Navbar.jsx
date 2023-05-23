d98b1f64c0d23882ffdaddfc390ed2be0246a417
Dariusz Legizynski <d.legizynski@.com>
2023-05-23 22:17:17 +0200

    set show header with dynamic height

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,41 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import logo from "../../assets/logo/Logo-remove-bg-small.png"
 
 import "./Navbar.css"
 import Burger from "../Burger/Burger"
 
 const Navbar = () => {
-	const [openBurger, setOpenBurger] = useState(false)
+	const [ openBurger, setOpenBurger ] = useState(false)
+
+	const listHeight = useRef()
+	const [ liHeight, setLiHeight ] = useState(0)
+
+  const navbarHeight = useRef()
+  const [ headerHeight, setHeaderHeight ] = useState(0)
+
+	console.log({ liHeight })
+  console.log({ openBurger })
+
+	useEffect(() => {
+		if (!listHeight?.current?.clientHeight || !navbarHeight?.current?.clientHeight) return
+
+		setLiHeight(listHeight?.current?.clientHeight)
+    setHeaderHeight(navbarHeight?.current?.clientHeight)
+	}, [listHeight?.current?.clientHeight, navbarHeight?.current?.clientHeight])
 
 	return (
-		<nav className="navbar">
+		<nav ref={navbarHeight} className="navbar">
 			<img src={logo} />
-			<li>
+			<li ref={listHeight} style={{ top: openBurger ? headerHeight : -liHeight}}>
 				<ul>Home</ul>
 				<ul>Kontakt</ul>
 				<ul>Leistungsumfang</ul>
 				<ul>Preise</ul>
 				<ul>Über mich</ul>
 			</li>
-			<li onClick={() => setOpenBurger(!openBurger)}>
+			<div className="navbar__burger" onClick={() => setOpenBurger(!openBurger)}>
 				<Burger isOpen={openBurger ? "--open" : ""} />
-			</li>
+			</div>
 		</nav>
 	)
 }

13610a484c8fc6c6b4be2a2f881711646e290ffa
Dariusz Legizynski <d.legizynski@.com>
2023-05-23 10:32:04 +0200

    add home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useRef } from "react"
+import { LocomotiveScrollProvider } from "react-locomotive-scroll"
+
+import Home from "./components/Home"
+
+import "locomotive-scroll/dist/locomotive-scroll.css"
 import "./App.css"
 
-function App() {
-	return <article>Main Page</article>
+const App = () => {
+	const containerRef = useRef(null)
+
+	return (
+		<LocomotiveScrollProvider
+			options={{
+				smooth: true,
+				// ... all available Locomotive Scroll instance options
+				smartphone: {
+					smooth: true,
+				},
+				tablet: {
+					smooth: true,
+				},
+			}}
+			watch={
+				[
+					//..all the dependencies you want to watch to update the scroll.
+					//  Basicaly, you would want to watch page/location changes
+					//  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+				]
+			}
+			containerRef={containerRef}
+		>
+			<main data-scroll-container ref={containerRef}>
+				<Home />
+			</main>
+		</LocomotiveScrollProvider>
+	)
 }
 
 export default App
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,13 @@
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <section className='home'>
+      <h1>Video</h1>
+      <h1>Logo</h1>
+      <h1>NavBar</h1>
+    </section>
+  )
+}
+
+export default Home

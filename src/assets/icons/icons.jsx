b218bea33cdb23b92c868b6b32a9fa8746efd986
Dariusz Legizynski <d.legizynski@.com>
2023-05-26 21:35:30 +0200

    clear process of svg line follower

--- src/assets/icons/icons.jsx
+++ src/assets/icons/icons.jsx
@@ -9,6 +9,11 @@ const IconItem = ({ type, fillColor, width, height, classType }) => {
 					<line x1="1.5" y1="900" x2="1.5" y2="1200" stroke="#BDFBFF" strokeWidth="3" />
 				</svg>
 			) : null}
+			{type === "brezier-line" ? (
+				<svg width={width} height={height} viewBox="0 0 127 1278" fill={fillColor} xmlns="http://www.w3.org/2000/svg" preserveAspectRatio='xMidYMax meet'>
+					<path className={classType} d="M60 0V49C60 64.6667 58.2 97.8 51 105C42 114 4 155 3 196C2.2 228.8 40.6667 262.333 60 275C75.6667 286.333 110.2 317.6 123 352C135.8 386.4 86.3333 439.667 60 462C41 473 3 505.6 3 548C3 590.4 41 621.667 60 632C81 642.333 123 676.4 123 730C123 783.6 81 815 60 824C36.6667 834.667 -7.4 870 3 926C13.4 982 45.3333 1006.67 60 1012C75.6667 1020.67 110.2 1048.6 123 1091C135.8 1133.4 97.6667 1176.67 77 1193C71.3333 1199.67 60 1217.6 60 1236C60 1254.4 60 1271.67 60 1278" stroke="black"/>
+				</svg>
+			) : null}
 		</>
 	)
 }
--- src/sections/Process/Process.jsx
+++ src/sections/Process/Process.jsx
@@ -1,66 +1,34 @@
 import { motion } from "framer-motion"
 
-import IconItem from '../../assets/icons/icons'
-
 import "./Process.css"
 
 const Process = () => {
 
-  window.onload = () => {
-    let path = document.querySelector("line")
-    console.log(path)
-    let pathLength = document.querySelector("line").getTotalLength()
-  
-    console.log(path, pathLength)
-  
-    path.style.strokeDasharray = pathLength + " " + pathLength
-  }
-
-
-  const container = {
+	const container = {
 		hidden: {
-      opacity: 0,
-      scale: 0,
-    },
-		visible:{
-      opacity: 1,
-      scale: 1,
-    },
+			opacity: 0,
+			scale: 0,
+		},
+		visible: {
+			opacity: 1,
+			scale: 1,
+		},
 	}
 
 	return (
 		<article className="process">
-			<section className="process__line-container">
-      <IconItem type="straight-line" classType="process__svg" />
-      </section>
-      <motion.p
-      className='process__show'
-      variants={container}
-      initial="hidden"
-      whileInView="visible"
-      transition={{duration: 0.6}}
-      >HELLO</motion.p>
-      <motion.p
-      className='process__show'
-      variants={container}
-      initial="hidden"
-      whileInView="visible"
-      transition={{duration: 0.6}}
-      >HELLO</motion.p>
-      <motion.p
-      className='process__show'
-      variants={container}
-      initial="hidden"
-      whileInView="visible"
-      transition={{duration: 0.6}}
-      >HELLO</motion.p>
-      <motion.p
-      className='process__show'
-      variants={container}
-      initial="hidden"
-      whileInView="visible"
-      transition={{duration: 0.6}}
-      >HELLO</motion.p>
+			<motion.p className="process__show" variants={container} initial="hidden" whileInView="visible" transition={{ duration: 0.6 }}>
+				HELLO
+			</motion.p>
+			<motion.p className="process__show" variants={container} initial="hidden" whileInView="visible" transition={{ duration: 0.6 }}>
+				HELLO
+			</motion.p>
+			<motion.p className="process__show" variants={container} initial="hidden" whileInView="visible" transition={{ duration: 0.6 }}>
+				HELLO
+			</motion.p>
+			<motion.p className="process__show" variants={container} initial="hidden" whileInView="visible" transition={{ duration: 0.6 }}>
+				HELLO
+			</motion.p>
 		</article>
 	)
 }

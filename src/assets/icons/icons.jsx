98da35474477c8c26b3f9b239986fe47a1f77d84
Dariusz Legizynski <d.legizynski@.com>
2023-05-28 22:58:23 +0200

    add line svg to project

--- src/assets/icons/icons.jsx
+++ src/assets/icons/icons.jsx
@@ -1,4 +1,4 @@
-const IconItem = ({ type, fillColor, width, height, classType }) => {
+const IconItem = ({ type, fillColor, strokeColor, width, height, classType }) => {
 	return (
 		<>
 			{type === "straight-line" ? (
@@ -12,6 +12,11 @@ const IconItem = ({ type, fillColor, width, height, classType }) => {
 			{type === "brezier-line" ? (
 				<svg width={width} height={height} viewBox="0 0 127 1278" fill={fillColor} xmlns="http://www.w3.org/2000/svg" preserveAspectRatio='xMidYMax meet'>
 					<path className={classType} d="M60 0V49C60 64.6667 58.2 97.8 51 105C42 114 4 155 3 196C2.2 228.8 40.6667 262.333 60 275C75.6667 286.333 110.2 317.6 123 352C135.8 386.4 86.3333 439.667 60 462C41 473 3 505.6 3 548C3 590.4 41 621.667 60 632C81 642.333 123 676.4 123 730C123 783.6 81 815 60 824C36.6667 834.667 -7.4 870 3 926C13.4 982 45.3333 1006.67 60 1012C75.6667 1020.67 110.2 1048.6 123 1091C135.8 1133.4 97.6667 1176.67 77 1193C71.3333 1199.67 60 1217.6 60 1236C60 1254.4 60 1271.67 60 1278" stroke="black"/>
+				</svg>
+			) : null}
+			{type === "vector" ? (
+				<svg width={width} height={height} viewBox="0 0 52 365" fill={fillColor} xmlns="http://www.w3.org/2000/svg">
+				<path d="M26 314C10.5 314 1 322.5 1 338.5C1 354.5 10 364.5 26 364.5M26 364.5C42 364.5 50.9016 356.4 50.5 338.5C50.0984 320.6 27.3356 321.68 26 338.5V364.5ZM26 0V314.5" stroke="black"/>
 				</svg>
 			) : null}
 		</>
--- src/sections/Process/Process.jsx
+++ src/sections/Process/Process.jsx
@@ -1,5 +1,7 @@
 import { motion } from "framer-motion"
 
+import DrawSVG from "../../components/drawSVG/drawSVG"
+
 import welcomeImage from "../../assets/illustrations/001_undraw_welcome_re_h3d9.svg"
 import examImage from "../../assets/illustrations/002_undraw_medicine_b-1-ol.svg"
 import protocolImage from "../../assets/illustrations/003_undraw_done_checking_re_6vyx.svg"
@@ -8,7 +10,6 @@ import therapyImage from "../../assets/illustrations/004_undraw_options_re_9vxh.
 import "./Process.css"
 
 const Process = () => {
-
 	const container = {
 		hidden: {
 			opacity: 0,
@@ -22,31 +23,44 @@ const Process = () => {
 
 	return (
 		<article className="process">
-			<section className="process__show" >
-        <motion.div variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2 }}>
-          <img className='process__img' src={welcomeImage} alt="image of a couple in love" />
-        </motion.div>
-        <motion.p variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2, delay: .4 }}>Zu Beginn steht ein ausführliches Gespräch mit speziell zugeschnittenen Fragen zur vollen Erfassung Ihrer Beschwerden.</motion.p>
+			<section className="process__container">
+				<div className="process__svg-container">
+					<DrawSVG />
+				</div>
 			</section>
-			<section className="process__show" >
-      <motion.p variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2, delay: .4 }}>Ergänzend können Tests sowie eine Tastuntersuchung im Becken-, Bauch-, Rücken- und/oder Hüftbereich sowie dem Beckenboden gemacht werden, um noch genauer auf Ihre Bedürfnisse einzugehen.</motion.p>
-        <motion.div variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2 }}>
-          <img className='process__img' src={examImage} alt="image of a couple in love" />
-        </motion.div>
-        
+			<section className="process__show">
+				<motion.div variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2 }}>
+					<img className="process__img" src={welcomeImage} alt="image of a couple in love" />
+				</motion.div>
+				<motion.p variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2, delay: 0.4 }}>
+					Zu Beginn steht ein ausführliches Gespräch mit speziell zugeschnittenen Fragen zur vollen Erfassung Ihrer Beschwerden.
+				</motion.p>
 			</section>
-			<section className="process__show" >
-        <motion.div variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2 }}>
-          <img className='process__img' src={protocolImage} alt="image of a couple in love" />
-        </motion.div>
-        <motion.p variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2, delay: .4 }}>Je nach Beschwerdebild kann ein Protokoll über Ihre Blase oder Ihren Darm in Form eines entsprechenden Tagebuches noch mehr Aussage über die Herkunft der eingetretenen Veränderung geben.</motion.p>
+			<section className="process__show">
+				<motion.p variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2, delay: 0.4 }}>
+					Ergänzend können Tests sowie eine Tastuntersuchung im Becken-, Bauch-, Rücken- und/oder Hüftbereich sowie dem Beckenboden gemacht werden, um noch
+					genauer auf Ihre Bedürfnisse einzugehen.
+				</motion.p>
+				<motion.div variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2 }}>
+					<img className="process__img" src={examImage} alt="image of a couple in love" />
+				</motion.div>
 			</section>
-			<section className="process__show" >
-      <motion.p variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2, delay: .4 }}>Die Therapie wird daraufhin individuell ausgewählt und während des gesamten Behandlungsverlaufes adaptiert und angepasst.</motion.p>
-        <motion.div variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2 }}>
-          <img className='process__img' src={therapyImage} alt="image of a couple in love" />
-        </motion.div>
-        
+			<section className="process__show">
+				<motion.div variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2 }}>
+					<img className="process__img" src={protocolImage} alt="image of a couple in love" />
+				</motion.div>
+				<motion.p variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2, delay: 0.4 }}>
+					Je nach Beschwerdebild kann ein Protokoll über Ihre Blase oder Ihren Darm in Form eines entsprechenden Tagebuches noch mehr Aussage über die Herkunft
+					der eingetretenen Veränderung geben.
+				</motion.p>
+			</section>
+			<section className="process__show">
+				<motion.p variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2, delay: 0.4 }}>
+					Die Therapie wird daraufhin individuell ausgewählt und während des gesamten Behandlungsverlaufes adaptiert und angepasst.
+				</motion.p>
+				<motion.div variants={container} initial="hidden" whileInView="visible" transition={{ duration: 1.2 }}>
+					<img className="process__img" src={therapyImage} alt="image of a couple in love" />
+				</motion.div>
 			</section>
 		</article>
 	)

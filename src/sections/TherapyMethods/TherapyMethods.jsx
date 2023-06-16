5782c3370423a603d168f77aafd845f33a668d93
Dariusz Legizynski <d.legizynski@.com>
2023-06-16 12:33:16 +0200

    moved list to cards

--- src/sections/TherapyMethods/TherapyMethods.jsx
+++ src/sections/TherapyMethods/TherapyMethods.jsx
@@ -1,7 +1,7 @@
+import CardMethod from '../../components/CardMethod/CardMethod'
 import "./TherapyMethods.css"
 
 const TherapyMethods = () => {
-
 	// useEffect(() => {
 	// 	const pin = gsap.fromTo(
 	// 		sectionRef.current,
@@ -34,26 +34,17 @@ const TherapyMethods = () => {
 		<article className="therapy-methods">
 			<section className="therapy-methods__title">
 				<h1>Therapiemethoden</h1>
-				<h2>je nach Beschwerdebild</h2>
+				<span>je nach Beschwerdebild</span>
 			</section>
 			<section className="therapy-methods__wrapper">
-					<ul className="therapy-methods__list">
-						<li>Kräftigen oder entspannen der Beckenboden-, Bauch-, Rücken- und/oder Hüftmuskulatur</li>
-						<li>
-							<p>Dehnungsübungen</p>
-							<p>Atemübungen</p>
-							<p>Entspannungsübungen</p>
-							<p>Elektrotherapie</p>
-							<p>Heimprogramm</p>
-						</li>
-						<li>Mobilisation von Gelenken im Hüft-, Becken- und/oder Rückenbereich</li>
-						<li>Passive Behandlungstechniken wie Faszien- oder Viszeraltechniken</li>
-						<li>Narbenbehandlung im Beckenboden-, Bauch- oder Rückenbereich</li>
-						<li>Biofeedback für den Beckenboden und die Bauchmuskulatur mittels Echtzeitultraschall oder einem klassischen Biofeedbackgerät</li>
-						<li>Erkennen und Bewältigen von typischen Belastungssituationen in Alltag, Sport sowie Beruf</li>
-						<li>Tipps und Support für den optimalen Umgang nach Operationen im Becken- oder Bauchbereich</li>
-						<li>Tipps und Support für den optimalen Umgang mit der Blase und dem Darm</li>
-					</ul>
+				<ul className="therapy-methods__list">
+					<CardMethod text1={"Kräftigen oder entspannen der Beckenboden-, Bauch-, Rücken- und/oder Hüftmuskulatur"} text2={"Mobilisation von Gelenken im Hüft-, Becken- und/oder Rückenbereich"} hr />
+					<CardMethod text1={"Dehnungsübungen"} text2={"Atemübungen"} text3={"Entspannungsübungen"} text4={"Elektrotherapie"} text5={"Heimprogramm"} />
+					<CardMethod text1={"Passive Behandlungstechniken wie Faszien- oder Viszeraltechniken"} text2={"Narbenbehandlung im Beckenboden-, Bauch- oder Rückenbereich"} />
+					<CardMethod text1={"Biofeedback für den Beckenboden und die Bauchmuskulatur mittels Echtzeitultraschall oder einem klassischen Biofeedbackgerät"} />
+					<CardMethod text1={"Erkennen und Bewältigen von typischen Belastungssituationen in Alltag, Sport sowie Beruf"} />
+					<CardMethod text1={"Tipps und Support für den optimalen Umgang:"} text2={"nach Operationen im Becken- oder Bauchbereich"} text3={"mit der Blase und dem Darm"} />
+				</ul>
 			</section>
 		</article>
 	)

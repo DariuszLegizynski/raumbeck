22375fb68884dbf2821dc4e059239875267cca67
Dariusz Legizynski <d.legizynski@.com>
2023-06-21 22:34:52 +0200

    remove unused icons

--- src/components/CardMethod/CardMethod.jsx
+++ src/components/CardMethod/CardMethod.jsx
@@ -1,13 +1,15 @@
+import IconItem from '../../assets/icons/icons'
 import "./CardMethod.css"
 
-const CardMethod = ({text1, text2, text3, text4, text5, hr}) => {
+const CardMethod = ({text1, text2, text3, text4, text5, imgSrc, imgAlt, hr}) => {
 	
   
   
   return (
 		<li className="card-method">
+      {imgSrc ? <img className="card-method__img" src={imgSrc} alt={imgAlt} /> : null}
 			{text1 ? <p>{text1}</p> : null}
-      {hr ? <hr /> : null}
+      {hr ? <hr className="card-method__horizontal-line" /> : null}
 			{text2 ? <p>{text2}</p> : null}
       {text3 ? <p>{text3}</p> : null}
       {text4 ? <p>{text4}</p> : null}
--- src/sections/TherapyMethods/TherapyMethods.jsx
+++ src/sections/TherapyMethods/TherapyMethods.jsx
@@ -1,6 +1,13 @@
 import CardMethod from '../../components/CardMethod/CardMethod'
 import "./TherapyMethods.css"
 
+import backpain from "../../assets/illustrations/back-pain-woman-svgrepo-com.svg"
+import gymnastics from "../../assets/illustrations/woman-doing-gymnastics-at-home-svgrepo-com.svg"
+import treatment from "../../assets/illustrations/nurse-svgrepo-com.svg"
+import feedback from "../../assets/illustrations/medical-examination-female-svgrepo-com.svg"
+import stressSituation from "../../assets/illustrations/a-clerk-chased-by-a-phone-call-for-inquiries-svgrepo-com.svg"
+import happyWomen from "../../assets/illustrations/happy-woman-svgrepo-com.svg"
+
 const TherapyMethods = () => {
 	// useEffect(() => {
 	// 	const pin = gsap.fromTo(
@@ -38,12 +45,12 @@ const TherapyMethods = () => {
 			</section>
 			<section className="therapy-methods__wrapper">
 				<ul className="therapy-methods__list">
-					<CardMethod text1={"Kräftigen oder entspannen der Beckenboden-, Bauch-, Rücken- und/oder Hüftmuskulatur"} text2={"Mobilisation von Gelenken im Hüft-, Becken- und/oder Rückenbereich"} hr />
-					<CardMethod text1={"Dehnungsübungen"} text2={"Atemübungen"} text3={"Entspannungsübungen"} text4={"Elektrotherapie"} text5={"Heimprogramm"} />
-					<CardMethod text1={"Passive Behandlungstechniken wie Faszien- oder Viszeraltechniken"} text2={"Narbenbehandlung im Beckenboden-, Bauch- oder Rückenbereich"} />
-					<CardMethod text1={"Biofeedback für den Beckenboden und die Bauchmuskulatur mittels Echtzeitultraschall oder einem klassischen Biofeedbackgerät"} />
-					<CardMethod text1={"Erkennen und Bewältigen von typischen Belastungssituationen in Alltag, Sport sowie Beruf"} />
-					<CardMethod text1={"Tipps und Support für den optimalen Umgang:"} text2={"nach Operationen im Becken- oder Bauchbereich"} text3={"mit der Blase und dem Darm"} />
+					<CardMethod imgSrc={backpain} imgAlt="women with backpain" text1={"Kräftigen oder entspannen der Beckenboden-, Bauch-, Rücken- und/oder Hüftmuskulatur"} text2={"Mobilisation von Gelenken im Hüft-, Becken- und/oder Rückenbereich"} hr />
+					<CardMethod imgSrc={gymnastics} imgAlt="women doing gimnastics" text1={"Dehnungsübungen"} text2={"Atemübungen"} text3={"Entspannungsübungen"} text4={"Elektrotherapie"} text5={"Heimprogramm"} />
+					<CardMethod imgSrc={treatment} imgAlt="women giving treatment" text1={"Passive Behandlungstechniken wie Faszien- oder Viszeraltechniken"} text2={"Narbenbehandlung im Beckenboden-, Bauch- oder Rückenbereich"} />
+					<CardMethod imgSrc={feedback} imgAlt="women getting medical feedback" text1={"Biofeedback für den Beckenboden und die Bauchmuskulatur mittels Echtzeitultraschall oder einem klassischen Biofeedbackgerät"} />
+					<CardMethod imgSrc={stressSituation} imgAlt="person in a stressfull situation" text1={"Erkennen und Bewältigen von typischen Belastungssituationen in Alltag, Sport sowie Beruf"} />
+					<CardMethod imgSrc={happyWomen} imgAlt="happy women" text1={"Tipps und Support für den optimalen Umgang:"} text2={"nach Operationen im Becken- oder Bauchbereich"} text3={"mit der Blase und dem Darm"} />
 				</ul>
 			</section>
 		</article>

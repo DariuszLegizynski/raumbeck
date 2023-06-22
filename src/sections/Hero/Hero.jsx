51655ab7781d2db0c5a6471913d1a06ffb2efdd7
Dariusz Legizynski <d.legizynski@.com>
2023-06-22 22:54:01 +0200

    fixes scroll trigger

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,37 +1,15 @@
 import hero from "../../assets/images/AboutMe.jpg"
-import { useRef, useEffect, useState } from "react"
+import { useState } from "react"
 import { motion, useScroll } from "framer-motion"
 
 import "./Hero.css"
 
 const Hero = () => {
-	const [ scrollPosition, setScrollPosition ] = useState(85)
+  const { scrollYProgress } = useScroll()
 
-	const letterA1 = useRef()
-	const letterN1 = useRef()
-	const letterN2 = useRef()
-	const letterA2 = useRef()
-	const letterM = useRef()
-	const letterE = useRef()
-	const letterH = useRef()
-	const letterR = useRef()
+	const [ yPos, setYPos ] = useState(0)
 
-	const sentence = useRef()
-
-	const { scrollYProgress } = useScroll({
-		target: letterA1
-	})
-
-	useEffect(() => {
-		setScrollPosition(scrollYProgress.current)
-	}, [scrollYProgress])
-
-	// gsap.registerPlugin(ScrollTrigger)
-	// const speed = 20
-
-	// const parallax = useRef()
-	console.log(scrollPosition)
-
+	scrollYProgress.onChange(val => setYPos(val))
 
 	const container = {
 		hidden: { opacity: 0 },
@@ -45,10 +23,8 @@ const Hero = () => {
 	}
 
 	const item = {
-		initial: { y: '0px'},
 		hidden: { opacity: 0 },
 		show: { opacity: 1 },
-		animate:{y: scrollYProgress}
 	}
 
 	return (
@@ -56,36 +32,36 @@ const Hero = () => {
 			<img src={hero} alt="therapist" />
 			<motion.section  className="hero__text" variants={container} initial="hidden" animate="show">
 				<div className="hero__title">
-					<motion.h1 ref={letterA1} className="green" variants={item}>
+					<motion.h1 style={{ y: -yPos * 2200}} className="green" variants={item}>
 						A
 					</motion.h1>
-					<motion.h1 ref={letterN1} className="green" variants={item}>
+					<motion.h1 style={{ y: -yPos * 2400}} className="green" variants={item}>
 						n
 					</motion.h1>
-					<motion.h1 ref={letterN2} className="green" variants={item}>
+					<motion.h1 style={{ y: -yPos * 2600}} className="green" variants={item}>
 						n
 					</motion.h1>
-					<motion.h1 ref={letterA2} className="green" variants={item}>
+					<motion.h1 style={{ y: -yPos * 2800}} className="green" variants={item}>
 						a
 					</motion.h1>
-					<motion.h1 variants={item}>&nbsp;</motion.h1>
-					<motion.h1 ref={letterM} className="grey" variants={item}>
+					<motion.h1 style={{ y: -yPos * 3000}} variants={item}>&nbsp;</motion.h1>
+					<motion.h1 style={{ y: -yPos * 2800}} className="grey" variants={item}>
 						M
 					</motion.h1>
-					<motion.h1 ref={letterE} className="grey" variants={item}>
+					<motion.h1 style={{ y: -yPos * 2600}} className="grey" variants={item}>
 						e
 					</motion.h1>
-					<motion.h1 ref={letterH} className="grey" variants={item}>
+					<motion.h1 style={{ y: -yPos * 2400}} className="grey" variants={item}>
 						h
 					</motion.h1>
-					<motion.h1 ref={letterR} className="grey" variants={item}>
+					<motion.h1 style={{ y: -yPos * 2200}} className="grey" variants={item}>
 						r
 					</motion.h1>
 				</div>
 				<motion.p
-					ref={sentence}
 					className="grey"
 					variants={item}
+					style={{ y: -yPos * 2000}}
 				>
 					--- Physiotherapeutin
 				</motion.p>

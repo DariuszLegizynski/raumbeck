af54c15ce7ed0a1ce9a143732cc94e989166ee98
Dariusz Legizynski <DLegizynski@anexia-it.com>
2023-09-29 10:53:15 +0200

    add icons for services section

--- src/components/ReasonCard/ReasonCard.jsx
+++ src/components/ReasonCard/ReasonCard.jsx
@@ -1,7 +1,6 @@
 import "./ReasonCard.css"
 
-const ReasonCard = ({title, reasonList}) => {
-
+const ReasonCard = ({title, reasonList, image}) => {
   const renderedReasons = reasonList.map((reason, id) => {
     return (
       <p className="reason-card__reason" key={`${id}${reason}`}>{reason}</p>
@@ -10,6 +9,9 @@ const ReasonCard = ({title, reasonList}) => {
 
 	return (
 		<article className="reason-card">
+			<section>
+				<img className="reason-card__img" src={image} alt={`health issue`} />
+			</section>
 			<h3>{title}</h3>
 			<section className="reason-card__reasons">
 				{renderedReasons}
--- src/sections/Reasons/Reasons.jsx
+++ src/sections/Reasons/Reasons.jsx
@@ -1,11 +1,17 @@
 import ReasonCard from '../../components/ReasonCard/ReasonCard'
 import "./Reasons.css"
 
+import plaster from "../../assets/icons/health/goodrx-svgrepo-com.svg"
+import walkHuman from "../../assets/icons/health/health-sync-svgrepo-com.svg"
+import crossShield from "../../assets/icons/health/librehealth-svgrepo-com.svg"
+import heartFolder from "../../assets/icons/health/mychart-svgrepo-com.svg"
+
 const Reasons = () => {
 	
   const cardList = [
     {
       id:1,
+      image: walkHuman,
       category: "Harninkontinenz",
       reasons: [
           "Harn- oder Stuhlverlust",
@@ -17,6 +23,7 @@ const Reasons = () => {
     },
     {
       id:2,
+      image: plaster,
       category: "Postoperative Versorgung",
       reasons: [
           "Gebärmutterentfernung",
@@ -29,6 +36,7 @@ const Reasons = () => {
     },
     {
       id:3,
+      image: heartFolder,
       category: "Schwangerschaft und Wochenbett",
       reasons: [
         "Schwangerschaftsbeschwerden",
@@ -37,6 +45,7 @@ const Reasons = () => {
     },
     {
       id:4,
+      image: crossShield,
       category: "Sexuelle Gesundheit",
       reasons: [
         "Schmerzen beim Geschlechtsverkehr",
@@ -48,7 +57,7 @@ const Reasons = () => {
 
     const renderedListOfReasonCards = cardList.map(card => {
       return (
-        <ReasonCard key={card.id} title={card.category} reasonList={card.reasons} />
+        <ReasonCard key={card.id} title={card.category} reasonList={card.reasons} image={card.image} />       
       )
     })
 

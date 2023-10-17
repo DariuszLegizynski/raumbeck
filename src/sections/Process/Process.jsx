922c01625ac843dfaa074ca0c3ecc33f591badaf
Dariusz Legizynski <DLegizynski@anexia-it.com>
2023-10-17 14:33:20 +0200

    add chessboard style

--- src/sections/Process/Process.jsx
+++ src/sections/Process/Process.jsx
@@ -60,7 +60,7 @@ const Process = () => {
 				<h1>Erstgespräch</h1>
 				<div className="hr-line" />
 			</section>
-			<img src={meeting} alt="therapist" />
+			<img className="process__hero-image" src={meeting} alt="therapist" />
 			<section className="process__body">
 				<section className="process__container flex-center">
 					<div className="flex-center">
@@ -74,6 +74,8 @@ const Process = () => {
 						imgAlt="a couple in love"
 						text="Zu Beginn steht ein ausführliches Gespräch mit speziell zugeschnittenen Fragen zur vollen Erfassung Ihrer Beschwerden."
 					/>
+					<div className='process__item-placeholder'>&nbsp;</div>
+					<div className='process__item-placeholder'>&nbsp;</div>
 					<ProcessItem
 						addToRef={addToRefs}
 						imgSrc={examImage}
@@ -88,6 +90,8 @@ const Process = () => {
 						text="Je nach Beschwerdebild kann ein Protokoll über Ihre Blase oder Ihren Darm in Form eines entsprechenden Tagebuches noch mehr Aussage über die
 						Herkunft der eingetretenen Veränderung geben."
 					/>
+					<div className='process__item-placeholder'>&nbsp;</div>
+					<div className='process__item-placeholder'>&nbsp;</div>
 					<ProcessItem
 						addToRef={addToRefs}
 						imgSrc={therapyImage}

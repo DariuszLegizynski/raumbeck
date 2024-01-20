0f0e3c5ba3daef65d4709496a0bed151a3229127
Dariusz Legizynski <DLegizynski@anexia-it.com>
2024-01-20 18:30:14 +0100

    add styles for active component

--- src/pages/Prices/Prices.jsx
+++ src/pages/Prices/Prices.jsx
@@ -1,15 +1,22 @@
 import "./Prices.css"
 
 import { useState } from "react"
+import { HashLink as Link } from "react-router-hash-link"
 
-const PriceCard = ({ time, handlePriceChange }) => {
+const PriceCard = ({ time, handlePriceChange, price }) => {
+	const baseClassName = "prices__card"
+	const handleActiveClass = `${baseClassName} ${price ? "isActive" : "notActive"}`
+
 	return (
-		<section className="prices__card" onClick={handlePriceChange}>
-			<section>
-				<p>{time} Min</p>
-				<p>lange Physiotherapie</p>
+		<section
+			className={handleActiveClass}
+			onClick={handlePriceChange}
+		>
+			<section className="prices__card__body">
+				<h3 style={price ? { color: "hsl(85, 55%, 51%)" } : {}}>{time} Min</h3>
+				<p>Physiotherapie</p>
 			</section>
-			<section>
+			<section className="prices__card__footer">
 				<p>Eine kurze und nette Beschreibung</p>
 			</section>
 		</section>
@@ -20,59 +27,68 @@ const SummaryCard = ({ time, price }) => {
 	return (
 		<section className="prices__card-summary">
 			<p>Preis:</p>
-			<section>
+			<section className="prices__card-summary__body">
 				<section>
 					<h2>{price} €</h2>
 					<p>Einmalige Zahlung</p>
 				</section>
-				<section>
-					<li>
-						<ul>{time} Min</ul>
-						<ul>Weshalb es sich Lohnt</ul>
-						<ul>Weshalb es sich Lohnt</ul>
-						<ul>Weshalb es sich Lohnt</ul>
-						<ul>Durchschnittliche Kostenrückerstattung: 27 €</ul>
-					</li>
-				</section>
+				<li className="prices__card-summary__list">
+					<ul>
+						<h3>{time} Min</h3>
+					</ul>
+					<ul>Weshalb es sich Lohnt</ul>
+					<ul>Weshalb es sich Lohnt</ul>
+					<ul>Weshalb es sich Lohnt</ul>
+					<ul>Durchschnittliche Kostenrückerstattung: 27 €</ul>
+				</li>
 			</section>
 		</section>
 	)
 }
 
 const Prices = () => {
-	const [ price, setPrice ] = useState(false)
+	const [price, setPrice] = useState(false)
 
 	return (
 		<article className="prices">
-			<section>
+			<section className="prices__title">
 				<h2>Preise</h2>
 				<p>Wählen Sie ein Tarif:</p>
 			</section>
 			<section>
-        <div onClick={() => setPrice(false)}>
-        <PriceCard
-					time={30}
-				/>
-        </div>
-        <div onClick={() => setPrice(true)}>
-        <PriceCard
-					time={45}
-				/>
-        </div>
-				{price ? (
-					<SummaryCard
+				<div onClick={() => setPrice(false)}>
+					<PriceCard
 						time={30}
-						price={55}
+						price={!price}
 					/>
-				) : (
+				</div>
+				<div onClick={() => setPrice(true)}>
+					<PriceCard
+						time={45}
+						price={price}
+					/>
+				</div>
+				{price ? (
 					<SummaryCard
 						time={45}
 						price={80}
 					/>
+				) : (
+					<SummaryCard
+						time={30}
+						price={55}
+					/>
 				)}
 			</section>
+			<Link
+				to="/#contact"
+				className="btn btn--green"
+			>
+				Kontakt
+			</Link>
 		</article>
 	)
 }
 
 export default Prices
+
--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,49 +1,101 @@
-import IconItem from '../../assets/icons/icons'
+import IconItem from "../../assets/icons/icons"
 import "./Contact.css"
 
 const Contact = () => {
 	return (
 		<div className="contact__bg">
-			<article className="contact" id="contact">
+			<article
+				className="contact"
+				id="contact"
+			>
 				<section className="contact__text">
 					<h1>Kontakt</h1>
 					<div className="hr-line" />
 					<p>Die Praxis liegt zentral im Zentrum von Wiener Neustadt am Ferdinand-Porsche-Ring Nr.&nbsp;7/Top&nbsp;1.</p>
 					<p>Parkmöglichkeiten sind direkt vor dem Gebäude in den umliegenden Kurzparkzonen oder in einem der naheliegenden Parkhäuser vorhanden.</p>
 					<p>
-						Es stehen für Sie ein Warteraum, eine Kaffeeküche, 2 moderne Behandlungsräume und ein großzügiger Trainingsraum mit allen wichtigen Trainingsgeräten zur
-						Verfügung.
+						Es stehen für Sie ein Warteraum, eine Kaffeeküche, 2 moderne Behandlungsräume und ein großzügiger Trainingsraum mit allen wichtigen Trainingsgeräten
+						zur Verfügung.
 					</p>
 					<p>Ich freue mich über Ihre Kontaktaufnahme.</p>
 				</section>
 				<section>
 					<ul className="contact__list">
-						<li className="contact__list__item"><IconItem type="phone" width="1.8rem" height="1.8rem" fillColor="none" /><a href="tel:068110494841">0681 / 104 94 841</a></li>
-						<li className="contact__list__item"><IconItem type="email" width="1.8rem" height="1.8rem" fillColor="none" /><a href="mailto:info@beckenraum.at">info@beckenraum.at</a></li>
-						<li className="contact__list__item"><IconItem type="home" width="1.8rem" height="1.8rem" fillColor="none" /><p>Ferdinand-Porsche-Ring 7 / Top 1</p></li>
-						<li className="contact__list__item"><b>&nbsp;</b><p>A-2700 Wiener Neustadt</p></li>
-						<li className="contact__list__item"><IconItem type="location" width="1.8rem" height="1.8rem" fillColor="none" /><a href="https://goo.gl/maps/L47fTuhFUsojYWn19">Auf Google Maps Öffnen</a></li>
+						<li className="contact__list__item">
+							<IconItem
+								type="phone"
+								width="1.8rem"
+								height="1.8rem"
+								fillColor="none"
+							/>
+							<a href="tel:068110494841">0681 / 104 94 841</a>
+						</li>
+						<li className="contact__list__item">
+							<IconItem
+								type="email"
+								width="1.8rem"
+								height="1.8rem"
+								fillColor="none"
+							/>
+							<a href="mailto:info@beckenraum.at">info@beckenraum.at</a>
+						</li>
+						<li className="contact__list__item">
+							<IconItem
+								type="home"
+								width="1.8rem"
+								height="1.8rem"
+								fillColor="none"
+							/>
+							<p>Ferdinand-Porsche-Ring 7 / Top 1</p>
+						</li>
+						<li className="contact__list__item">
+							<b>&nbsp;</b>
+							<p>A-2700 Wiener Neustadt</p>
+						</li>
+						<li className="contact__list__item">
+							<IconItem
+								type="location"
+								width="1.8rem"
+								height="1.8rem"
+								fillColor="none"
+							/>
+							<a href="https://goo.gl/maps/L47fTuhFUsojYWn19">Auf Google Maps Öffnen</a>
+						</li>
 					</ul>
 					<form className="contact__form">
-					<div className="contact__form__item">
-						<input type="text" placeholder="Name" required />
-					</div>
-					<div className="contact__form__item">
-						<input type="email" placeholder="E-mail" required />
-					</div>
-					<div className="contact__form__item">
-						<input type="number" placeholder="Telefon" />
-					</div>
-					<div className="contact__form__item">
-						<textarea placeholder="Nachricht" required />
-					</div>
-					<button className="btn btn--highlight">Senden</button>
+						<div className="contact__form__item">
+							<input
+								type="text"
+								placeholder="Name"
+								required
+							/>
+						</div>
+						<div className="contact__form__item">
+							<input
+								type="email"
+								placeholder="E-mail"
+								required
+							/>
+						</div>
+						<div className="contact__form__item">
+							<input
+								type="number"
+								placeholder="Telefon"
+							/>
+						</div>
+						<div className="contact__form__item">
+							<textarea
+								placeholder="Nachricht"
+								required
+							/>
+						</div>
+						<button className="btn btn--highlight">Senden</button>
 					</form>
 				</section>
 			</article>
 		</div>
-		
 	)
 }
 
 export default Contact
+

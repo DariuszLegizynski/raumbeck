070a040ed7b6c379475fdd6ae17210f471ee43c1
Dariusz Legizynski <DLegizynski@anexia-it.com>
2024-03-12 10:12:29 +0100

    add icons and text

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,3 +1,5 @@
+import IconItem from "../../assets/icons/icons"
+
 import "./About.css"
 
 import { useState } from 'react'
@@ -23,17 +25,50 @@ const About = () => {
 				</div>
 				<section className='about__title'>
 					<ul>
-						<li><h3 onClick={() => handleItemClick(1)}>MEINE GESCHICHTE</h3></li>
-						<li><h3 onClick={() => handleItemClick(2)}>WEITERBILDUNG</h3></li>
-						<li><h3 onClick={() => handleItemClick(3)}>BERUFLICHES</h3></li>
+						<li onClick={() => handleItemClick(1)}>
+							<IconItem
+								type='book'
+								width="2rem"
+								height="2rem"
+							/>
+							<h3>
+								MEINE GESCHICHTE
+							</h3>
+						</li>
+						<li onClick={() => handleItemClick(2)}>
+							<IconItem
+								type='knowledge'
+								width="2.4rem"
+								height="2.4rem"
+								fillColor="none"
+								strokeColor='#000000'
+							/>
+							<h3>
+								WEITERBILDUNG
+							</h3>
+						</li>
+						<li onClick={() => handleItemClick(3)}>
+							<IconItem
+								type='job'
+								width="2rem"
+								height="2rem"
+								strokeColor="#000000"
+								fillColor="#000000"
+							/>
+							<h3>
+								BERUFLICHES
+							</h3>
+						</li>
 					</ul>
 				</section>
-				{clickedItem === 1 ? (<section className='about__story'>
+				{clickedItem === 1 ? (
+				<section className='about__story'>
 					<span>Durch mein großes Interesse im gynäkologischen, urologischen & proktologischen Bereich begann ich mich ausgiebig für die physiotherapeutische Tätigkeit in diesem Zusammenhang zu beschäftigen. Dabei bekam ich die Möglichkeit, in darauf spezialisierten Praxen tätig zu sein und somit viel Erfahrung mit Patienten sowie im Austausch mit Arbeitskollegen und Ärzten sammeln zu dürfen. Für mich wurde das Thema zur sinnstiftenden Passion.</span>
 					<p>&nbsp;</p>
 					<span>Im Juni 2021 durfte ich durch die Geburt meines Sohnes selbst eigene Erfahrungen im Muttersein sammeln und freue mich seit August 2022 wieder als Physiotherapeutin tätig zu sein.</span>
 				</section>) : null}
-				{clickedItem === 2 ? (<section className='about__experience'>
+				{clickedItem === 2 ? (
+				<section className='about__experience'>
 					<h3>2009 – 2017</h3>
 					<span>Re.praxis für Physiotherapie, Schwerpunkt: Gynäkologie, Urologie & Proktologie</span>
 					<h3>2011 – 2017</h3>
@@ -41,102 +76,152 @@ const About = () => {
 					<h3>2017 – 2021</h3>
 					<span>Physiotherapie Praxis Frauensache & Männerthemen, Schwerpunkt: Gynäkologie, Urologie & Proktologie</span>
 				</section>) : null}
-				{clickedItem === 3 ? (<section className='about__work'>
+				{clickedItem === 3 ? (
+				<section className='about__work'>
 				<h3>Weiterbildungen mit Schwerpunkt Beckenbodentherapie:</h3>
-<p>Mai 2023</p>
+				<div>
+				<p>Mai 2023</p>
 
 <span>Beckenboden Special – Vulvodynie I & II</span>
-Jänner 2023
+				</div>
+					
+					<div>
+					<p>Jänner 2023</p>
 
-Viszerale Therapie – Läsionskette Verdauungstrakt – Wirbelsäule
-November 2022 
+<span>Viszerale Therapie – Läsionskette Verdauungstrakt – Wirbelsäule</span>
+					</div>
+					
+					<div>
+					<p>November 2022</p>
 
-Beckenboden Special – Echtzeit Ultraschall in der Beckenbodentherapie beim Mann
-Oktober 2020 
+					<span>Beckenboden Special – Echtzeit Ultraschall in der Beckenbodentherapie beim Mann</span>
+					</div>
 
-Beckenboden Special – Das hintere Kompartiment
-September 2020
+					<div>
+					<p>Oktober 2020</p>
 
-Beckenboden Special – Vaginale Palpation Therapie
-August 2020
+					<span>Beckenboden Special – Das hintere Kompartiment</span>
+					</div>
 
-Echtzeit-Ultraschallkurs in der Beckenbodentherapie
-Dezember 2019
+					<div>
+					<p>September 2020</p>
 
-Geburt, Wochenbett, Evidenz
-Juni 2019                                  
+<span>Beckenboden Special – Vaginale Palpation Therapie</span>
+					</div>
+					
+					<div>
+					<p>August 2020</p>
 
-SKENAR-Technologie – Basis-Seminar
-April 2019
+<span>Echtzeit-Ultraschallkurs in der Beckenbodentherapie</span>
+					</div>
+					
+					<div>
+					<p>Dezember 2019</p>
 
-Physiotherapie bei Kindern mit anorektalen und urogenitalen Funktionsstörungen
-Oktober 2017
+<span>Geburt, Wochenbett, Evidenz</span>
+					</div>
+					
+					<div>
+					<p>Juni 2019</p>
 
-Das Prostatakarzinom – Physiotherapeutische Behandlungsmöglichkeiten
-Mai 2017
+<span>SKENAR-Technologie – Basis-Seminar</span>
+					</div>
+					
+					<div>
+					<p>April 2019</p>
 
-Echtzeit-Ultraschallkurs in der Beckenbodentherapie
-Januar 2017
+<span>Physiotherapie bei Kindern mit anorektalen und urogenitalen Funktionsstörungen</span>
 
-Echtzeit-Ultraschall im Bereich Becken und Lendenwirbelsäule
-April 2014
-
-Physiotherapie bei anorektalen Dysfunktionen
-November 2013
-
-Grundlagen der Ernährung für eine optimierte physiotherapeutische Behandlung
-September 2011
-
-Nach der Geburt – Wochenbett und Rückbildung – Heller Konzept
-März 2011
-
-Beckenbodenrehabilitation beim Mann
-November 2010
-
-Beckenbodenfunktionen und -dysfunktionen, Physiotherapie nach dem Heller-Konzept
-September 2010 
-
-Chronische Beckenboden und Unterleibsbeschwerden
-Mai 2009
-
-Weiterbildungen mit Schwerpunkt Triggerpunkttherapie
-Oktober 2012
-
-Manuelle Triggerpunkttherapie MTP3
-November 2011
-
-Manuelle Triggerpunkt-Therapie MTP2
-Mai 2011
-
-Manuelle Triggerpunkt-Therapie MTP1
-Weiterbildungen mit Schwerpunkt Manualtherapie
-
-Oktober 2010
-
-Manuelle Therapie, Prüfungsvorbereitungskurs mit abschließender Prüfung zur Manualtherapeutin
-Februar 2010
-
-Manuelle Therapie, Aufbaukurs Hals-Brustwirbelsäule-Arm-Bereich
-Oktober 2009
-
-Manuelle Therapie, Aufbaukurs Lenden-Becken-Bein-Bereich
-März 2009
-
-Manuelle Therapie, Weichteil-Kurs
-November 2008
-
-Manuelle Therapie, Wirbelsäulen Kurs
-Juni 2008
-
-Manuelle Therapie, Extremitäten Kurs
-November 2008
-
-Kinesiology, Power Spiral Taping
-Weiterbildungen mit Schwerpunkt Lymphdrainagetherapie
-
-Oktober 2007
-
-Manuelle Lymphdrainage nach Dr. Vodder
+					</div>
+					
+					<div>
+					<p>Oktober 2017</p>
+					<span>Das Prostatakarzinom – Physiotherapeutische Behandlungsmöglichkeiten</span>
+					</div>
+					<div>
+						<p>Mai 2017</p>
+						<span>Echtzeit-Ultraschallkurs in der Beckenbodentherapie</span>
+					</div>
+					<div>
+						<p>Januar 2017</p>
+						<span>Echtzeit-Ultraschall im Bereich Becken und Lendenwirbelsäule</span>
+					</div>
+					<div>
+						<p>April 2014</p>
+						<span>Physiotherapie bei anorektalen Dysfunktionen</span>
+					</div>
+					<div>
+						<p>November 2013</p>
+						<span>Grundlagen der Ernährung für eine optimierte physiotherapeutische Behandlung</span>
+					</div>
+					<div>
+						<p>September 2011</p>
+						<span>Nach der Geburt – Wochenbett und Rückbildung – Heller Konzept</span>
+					</div>
+					<div>
+						<p>März 2011</p>
+						<span>Beckenbodenrehabilitation beim Mann</span>
+					</div>
+					<div>
+						<p>November 2010</p>
+						<span>Beckenbodenfunktionen und -dysfunktionen, Physiotherapie nach dem Heller-Konzept</span>
+					</div>
+					<div>
+						<p>September 2010</p>
+						<span>Chronische Beckenboden und Unterleibsbeschwerden</span>
+					</div>
+					<div>
+						<p>Mai 2009</p>
+						<span>Weiterbildungen mit Schwerpunkt Triggerpunkttherapie</span>
+					</div>
+					<div>
+						<p>Oktober 2012</p>
+						<span>Manuelle Triggerpunkttherapie MTP3</span>
+					</div>
+					<div>
+						<p>November 2011</p>
+						<span>Manuelle Triggerpunkt-Therapie MTP2</span>
+					</div>
+					<div>
+						<p>Mai 2011</p>
+						<span>Manuelle Triggerpunkt-Therapie MTP1</span>
+						
+					</div>
+					<h3>Weiterbildungen mit Schwerpunkt Manualtherapie</h3>
+					<div>
+						<p>Oktober 2010</p>
+						<span>Manuelle Therapie, Prüfungsvorbereitungskurs mit abschließender Prüfung zur Manualtherapeutin</span>
+					</div>
+					<div>
+						<p>Februar 2010</p>
+						<span>Manuelle Therapie, Aufbaukurs Hals-Brustwirbelsäule-Arm-Bereich</span>
+					</div>
+					<div>
+						<p>Oktober 2009</p>
+						<span>Manuelle Therapie, Aufbaukurs Lenden-Becken-Bein-Bereich</span>
+					</div>	
+					<div>
+						<p>März 2009</p>
+						<span>Manuelle Therapie, Weichteil-Kurs</span>
+					</div>
+					<div>
+						<p>November 2008</p>
+						<span>Manuelle Therapie, Wirbelsäulen Kurs</span>
+					</div>
+					<div>
+						<p>Juni 2008</p>
+						<span>Manuelle Therapie, Extremitäten Kurs</span>
+					</div>
+					<div>
+						<p>November 2008</p>
+						<span>Kinesiology, Power Spiral Taping</span>
+					</div>
+					<div>
+						<h3>Weiterbildungen mit Schwerpunkt Lymphdrainagetherapie</h3>
+						<p>Oktober 2007</p>
+						<span>Manuelle Lymphdrainage nach Dr. Vodder</span>
+					</div>
+					
 				</section>) : null}
 			</section>
 			
